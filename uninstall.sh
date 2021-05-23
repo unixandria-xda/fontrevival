4bf7e12305719bf48de24533959e22acbbefaed6
androidacy <admin@androidacy.com>
2021-05-23 08:27:50 -0400

    Fix: uninstall doesn't do proper cleanup
    Signed-off-by: androidacy <admin@androidacy.com>

--- uninstall.sh
+++ uninstall.sh
@@ -17,6 +17,13 @@ if [ -f "$INFO" ]; then
   done <"$INFO"
   rm -f "$INFO"
 fi
-setenforce 0
-rm -rf /sdcard/Fontifier
-setenforce 1
+echo "# FontManager Cleanup Script
+while test \"$(getprop sys.boot_completed)\" != \"1\"  && test ! -d /storage/emulated/0/Android ;
+do sleep 2;
+done
+rm -rf /storage/emulated/0/FontManager
+rm -rf /data/adb/service.d/fm-cleanup.sh
+exit 0" >/data/adb/service.d/fm-cleanup.sh
+chmod 755 /data/adb/service.d/fm-cleanup.sh
+sync && sleep 1
+reboot
